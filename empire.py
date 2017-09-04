1f81e3dae0b429cc1ace952ca3ddf43e5208df83
Rom Valme <rom.valme@gmail.com>
2017-09-04 06:40:03 -0400

    fixing directory structure, updating empire.py, finished about section

--- empire.py
+++ empire.py
@@ -3,14 +3,24 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("template.html", title="My title")
+        self.render("mp-index-18.html", title="My title")
+
+class AboutHandler(tornado.web.RequestHandler):
+    def get(self):
+	self.render("pages-about-1.html", title="My title")
 
 def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+    settings = {'debug': True }
+    handlers = [
+		(r"/", MainHandler),
+		(r"/pages-about-1.html", AboutHandler),
+		(r"/mp-index-18.html", MainHandler),
+		(r'/(.*)', tornado.web.StaticFileHandler, {'path': 'static'}),
+    ]
+    return tornado.web.Application(handlers, **settings)
+
 
 if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    tornado.ioloop.IOLoop.instance().start()
